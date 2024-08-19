934cead1aaa91e5f22c04913cfafe4e6cda8e2e2
sonalidoke11 <sonalidoke810@gmail.com>
2024-08-19 16:36:33 -0400

    added readme file

---
+++ README.md
@@ -0,0 +1,30 @@
+Description :
+
+Analyze and transform given data such that the output of your program is an array of objects.
+
+Project Link : https://github.com/sonalidoke11/SBA_308_Javascript_Fundamentals.git
+
+Technical Specification:
+1) Created function getLearnerData() which accepts arguments and process object data to calculate grades and display data in array of objects
+
+2) Declared variables using let and const 
+
+3) Used operators to perform calculations on variables and literals.
+
+4) Used strings, numbers, and Boolean values cached within variables.
+
+5) Used if/else and try/catch block
+
+6) Use of if/else, forEach, for in, for of loops
+
+7) Used control lookeyword 'continue
+
+8) Created and manipulated arrays and objects.
+
+9) Worked on array manipulation
+
+10) Created function for repeatative use
+
+11) outputs complete processed data as described without error
+
+12) Added project to git repository with frequent commits
--- src/fundamentals.js
+++ src/fundamentals.js
@@ -76,113 +76,115 @@ const LearnerSubmissions = [
   }
 ];
 
-// function to check whether AssignmentGroup does not belong to its course 
+// function to check whether AssignmentGroup does belong to its course 
 function isValidCourse(CourseInfo, AssignmentGroup) {
-if(CourseInfo.id === AssignmentGroup.course_id){return true} 
+  if(CourseInfo.id === AssignmentGroup.course_id){
+    return true
+  }else{
+    return false
+  } 
 }
 
 // getLearnerData function starts 
 function getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions) {
 
-const result = []; 
-let assignment_Scores = [];
-const assignments_lists = AssignmentGroup.assignments;
-const learnerData = {};
+  //Variables declaration
+    const result = []; 
+    let assignment_Scores = [];
+    const assignments_lists = AssignmentGroup.assignments;
+    const learnerData = {};
 
-// If an AssignmentGroup does not belong to its course (mismatching course_id), your program should throw an error, letting the user know that the input was invalid. Similar data validation should occur elsewhere within the program.
+  // If an AssignmentGroup does not belong to its course (mismatching course_id), your program should throw an error, letting the user know that the input was invalid. Similar data validation should occur elsewhere within the program.
 
-let isValid = isValidCourse(CourseInfo, AssignmentGroup)
-//console.log(isValid);
+    let isValid = isValidCourse(CourseInfo, AssignmentGroup)
+    //console.log(isValid);
 
 
-//error handeling if assignment not belong to course
-if (!isValid) {
-    throw new Error("Invalid input: Assignment is not available in this course.");
-}
-
-// looping through learnersSubmisions to areate learnersData array and calculate average
-for(const eachSubmission of LearnerSubmissions) {  
-
-    const learnerID = eachSubmission.learner_id;  //assigning value of learners ID
-    const assignmentID = eachSubmission.assignment_id;  //assigning value of assignment ID
-    const assignmentData = assignments_lists.find((assignment_details) => assignment_details.id === assignmentID);  //getting assignment details from assignment list with id
-
-    //check if assignment submission date is later that assignment due date
-    if (new Date(eachSubmission.submission.submitted_at) > new Date(assignmentData.due_at)) {
-      continue;
+  //error handeling if assignment not belong to course
+    if (!isValid) {
+        throw new Error("Invalid input: Assignment is not available in this course.");
     }
 
-    try{
-      //You should also account for potential errors in the data that your program receives. What if points_possible is 0? You cannot divide by zero. What if a value that you are expecting to be a number is instead a string? 
-      if (!(assignmentData.points_possible === 0 && typeof(eachSubmission.submission.score) !== "number")) {
-        learnerData[learnerID] = {
-              id: learnerID,
-              totalScore: 0,
-              totalWeight: 0,
+  // looping through learnersSubmisions to create learnersData array and calculate average
+    for(const eachSubmission of LearnerSubmissions) {  
+
+        const learnerID = eachSubmission.learner_id;  //assigning value of learners ID
+        const assignmentID = eachSubmission.assignment_id;  //assigning value of assignment ID
+        const assignmentData = assignments_lists.find((assignment_details) => assignment_details.id === assignmentID);  //getting assignment details from assignment list with id
+
+        //check if assignment submission date is later that assignment due date
+        if (new Date(eachSubmission.submission.submitted_at) > new Date(assignmentData.due_at)) {
+          continue;
         }
 
-        // Assigning value to variables
-          const pointsScore = eachSubmission.submission.score;
-          const totalPointsPossible = assignmentData.points_possible;  
+        try{
+          //You should also account for potential errors in the data that your program receives. What if points_possible is 0? You cannot divide by zero. What if a value that you are expecting to be a number is instead a string? 
+          if (!(assignmentData.points_possible === 0 && typeof(eachSubmission.submission.score) !== "number")) {
+            learnerData[learnerID] = {
+                  id: learnerID,
+                  totalScore: 0,
+                  totalWeight: 0,
+            }
 
-          
-          // creating array to push marks according to assignmentsID's
-          assignment_Scores.push ( {
-            id : learnerID,
-            assignment_id : assignmentID ,
-            marks : ((eachSubmission.submission.score/assignmentData.points_possible)+" (" +eachSubmission.submission.score + " / "+ assignmentData.points_possible + ")")
-          } )
-          
-          //Calculate total score and average
-          learnerData[learnerID].totalScore += pointsScore;
-          learnerData[learnerID].totalWeight += totalPointsPossible;
-          learnerData[learnerID].average = (pointsScore/totalPointsPossible)*100; 
-    
+            // Assigning value to variables
+              const pointsScore = eachSubmission.submission.score;
+              const totalPointsPossible = assignmentData.points_possible;  
 
+              
+            // creating array to push marks according to assignmentsID's
+              assignment_Scores.push ( {
+                id : learnerID,
+                assignment_id : assignmentID ,
+                marks : ((eachSubmission.submission.score/assignmentData.points_possible)+" (" +eachSubmission.submission.score + " / "+ assignmentData.points_possible + ")")
+              } )
+              
+            //Calculate total score and average
+              learnerData[learnerID].totalScore += pointsScore;
+              learnerData[learnerID].totalWeight += totalPointsPossible;
+              learnerData[learnerID].average = (pointsScore/totalPointsPossible)*100; 
+            }
+        }catch(error){
+          console.log(error);
         }
-    }catch(error){
-      console.log(error);
+    }   
+
+
+    for (const learnerID in learnerData) {
+        
+      //To display assignment score with assignment id
+        let result1
+        result1 = assignment_Scores.reduce(function(r, a) {
+          r[a.id] = r[a.id] || [];
+          r[a.id].push(a.marks);
+          return r;
+        }, Object.create(null));
+
+        
+      //combining result  to display as per output
+        const learnerResult = {
+          avg: learnerData[learnerID].average,
+          id: learnerID,
+        };  
+
+        for (let assignmentID in result1[learnerID]) {
+          let assignmentID1 = parseInt(assignmentID)+1;
+          learnerResult[assignmentID1] = result1[learnerID][assignmentID];
+        }
+
+        result.push(learnerResult);
     }
-}   
 
+    return result;
+  }
 
-for (const learnerID in learnerData) {
+  // Calling getLearerData function with parameters for data calculations
+    const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
+    console.log( result);
 
-    
-    let result1
-    result1 = assignment_Scores.reduce(function(r, a) {
-      r[a.id] = r[a.id] || [];
-      r[a.id].push(a.marks);
-      return r;
-    }, Object.create(null));
-
-    
-    
-    const learnerResult = {
-      avg: learnerData[learnerID].average,
-      id: learnerID,
-    };  
-
-    for (let assignmentID in result1[learnerID]) {
-      let assignmentID1 = parseInt(assignmentID)+1;
-      learnerResult[assignmentID1] = result1[learnerID][assignmentID];
-    }
-
-    result.push(learnerResult);
-}
-
-return result;
-}
-
-const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
-console.log( result);
-
-result.forEach(element => {
-  console.log("Details of student Id : " +element.id);
-  console.log(element)
-});
-
-
+    result.forEach(element => {
+      console.log("Details of student Id : " +element.id);
+      console.log(element)
+    });
 
 
 
